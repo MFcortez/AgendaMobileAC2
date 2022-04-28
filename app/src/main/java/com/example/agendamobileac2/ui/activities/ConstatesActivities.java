93d8988995076968430abc86fe444950da9d899c
Aluno <aluno@LI.LOCAL>
2022-04-28 20:24:58 -0300

    Initial commit

---
+++ app/src/main/java/com/example/agendamobileac2/ui/activities/ConstatesActivities.java
@@ -0,0 +1,5 @@
+package com.example.agendamobileac2.ui.activities;
+
+public interface ConstatesActivities {
+    String CHAVE_PERSONAGEM = "personagem";
+}
---
+++ app/src/main/java/com/example/agendamobileac2/ui/activities/FormularioPersonagemActivity.java
@@ -0,0 +1,108 @@
+package com.example.agendamobileac2.ui.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import com.example.agendamobileac2.R;
+import com.example.agendamobileac2.dao.PersonagemDAO;
+import com.example.agendamobileac2.model.Personagem;
+import com.github.rtoshiro.util.format.SimpleMaskFormatter;
+import com.github.rtoshiro.util.format.text.MaskTextWatcher;
+
+import static com.example.agendamobileac2.ui.activities.ConstatesActivities.CHAVE_PERSONAGEM;
+
+public class FormularioPersonagemActivity extends AppCompatActivity {
+    private static final String TITULO_APPBAR_EDITA_PERSONAGEM = "Editar o Personagem";
+    private static final String TITULO_APPBAR_NOVO_PERSONAGEM = "Novo Personagem";
+    private EditText campoNome;
+    private EditText campoNascimento;
+    private EditText campoAltura;
+    private final PersonagemDAO dao = new PersonagemDAO();
+    private Personagem personagem;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_formulario_personagem_menu_salvar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        int itemId = item.getItemId();
+        if (itemId == R.id.activity_formulario_personagem_menu_salvar) {
+            finalizarFormulario();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_personagem);
+        inicializacaoCampos();
+        //configuraBotaoSalvar();
+        carregaPersonagem();
+        checaPermissoes();
+    }
+
+    private void carregaPersonagem() {
+        Intent dados = getIntent();
+        if (dados.hasExtra(CHAVE_PERSONAGEM)) {
+            setTitle(TITULO_APPBAR_EDITA_PERSONAGEM);
+            personagem = (Personagem) dados.getSerializableExtra(CHAVE_PERSONAGEM);
+            preenchaCampos();
+        } else {
+            setTitle(TITULO_APPBAR_NOVO_PERSONAGEM);
+            personagem = new Personagem();
+        }
+    }
+    private void preenchaCampos() {
+        campoNome.setText(personagem.getNome());
+        campoAltura.setText(personagem.getAltura());
+        campoNascimento.setText(personagem.getNascimento());
+    }
+
+    private void finalizarFormulario()
+    {
+        preencherPersonagem();
+        if (personagem.IdValido()) {
+            dao.edita(personagem);
+            finish();
+        } else {
+            dao.salva(personagem);
+        }
+        finish();
+    }
+
+    private void inicializacaoCampos() {
+        campoNome = findViewById(R.id.editText_nome);
+        campoNascimento = findViewById(R.id.editText_nascimento);
+        campoAltura = findViewById(R.id.editText_altura);
+
+        SimpleMaskFormatter smfAltura = new SimpleMaskFormatter("N,NN");
+        MaskTextWatcher mtwAltura = new MaskTextWatcher(campoAltura, smfAltura);
+        campoAltura.addTextChangedListener(mtwAltura);
+
+        SimpleMaskFormatter smfNascimento = new SimpleMaskFormatter("N,NN");
+        MaskTextWatcher mtwNascimento = new MaskTextWatcher(campoNascimento, smfNascimento);
+        campoAltura.addTextChangedListener(mtwNascimento);
+    }
+
+    private void preencherPersonagem() {
+        String nome = campoNome.getText().toString();
+        String nascimento = campoNascimento.getText().toString();
+        String altura = campoAltura.getText().toString();
+
+        personagem.setNome(nome);
+        personagem.setAltura(altura);
+        personagem.setNascimento(nascimento);
+
+    }
+}
